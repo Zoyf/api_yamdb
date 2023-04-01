cfd31ae6c3e80741e95bc87f3907870654d49e5b
Виталий Залесов <vitaliyzalesov@yandex.ru>
2023-04-01 16:50:46 +0300

    Добавил урлы

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,20 +1,11 @@
-from django.shortcuts import get_object_or_404
-from rest_framework import viewsets
+from rest_framework.routers import DefaultRouter
 
-from serializers import ReviewSerializer
-from permissions import IsAdminOrModeratorOrReadOnly
-from reviews.models import Title
+from views import ReviewViewSet
 
 
-class ReviewViewSet(viewsets.ModelViewSet):
-    serializer_class = ReviewSerializer
-    permission_classes = [IsAdminOrModeratorOrReadOnly, ]
-
-    def get_queryset(self):
-        title_id = self.kwargs.get("title_id")
-        title = get_object_or_404(Title, id=title_id)
-        return title.reviews.all()
-
-    def perform_create(self, serializer):
-        title = get_object_or_404(Title, id=self.kwargs.get("title_id"))
-        serializer.save(author=self.request.user, title=title)
+router = DefaultRouter()
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews'
+)
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
 
-# Create your views here.
+from serializers import ReviewSerializer
+from permissions import IsAdminOrModeratorOrReadOnly
+from reviews.models import Title
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    permission_classes = [IsAdminOrModeratorOrReadOnly, ]
+
+    def get_queryset(self):
+        title_id = self.kwargs.get("title_id")
+        title = get_object_or_404(Title, id=title_id)
+        return title.reviews.all()
+
+    def perform_create(self, serializer):
+        title = get_object_or_404(Title, id=self.kwargs.get("title_id"))
+        serializer.save(author=self.request.user, title=title)
