cd13991bc1191dd91063f084999a36fd85db3e46
Анастасия Лосева <nastyprivetkyvem@gmail.com>
2023-04-02 07:48:31 +0300

    произведения, категории, жанры

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,9 +1,9 @@
 import datetime as dt
+
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
 
 from reviews.models import Category, Genre, Title
-
 
 
 class CategorySerializer(serializers.ModelSerializer):
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -110,5 +110,3 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
-
-DATE_INPUT_FORMATS = ['%Y']
