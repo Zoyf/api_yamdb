445a2682cd638a9176edd9bc419550609384be6b
anastasia-loseva <102997831+anastasia-loseva@users.noreply.github.com>
2023-04-04 16:53:39 +0300

    Добавление фильтрации

---
+++ api_yamdb/api/filters.py
@@ -0,0 +1,13 @@
+from django_filters import rest_framework
+from reviews.models import Title
+
+
+class TitleFilter(rest_framework.FilterSet):
+    category = rest_framework.CharFilter(field_name='category__slug')
+    name = rest_framework.CharFilter(field_name='name', lookup_expr='icontains')
+    genre = rest_framework.CharFilter(field_name='genre__slug')
+    year = rest_framework.NumberFilter()
+
+    class Meta:
+        model = Title
+        fields = ('category', 'genre', 'year', 'name',)
