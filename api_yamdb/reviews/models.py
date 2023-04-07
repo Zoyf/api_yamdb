04d3a67c5bfda02c6c6d2804f5b6654c61c9b48c
Виталий Залесов <vitaliyzalesov@yandex.ru>
2023-04-07 10:24:02 +0300

    Исправлены импорты

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,10 +1,7 @@
 from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.tokens import default_token_generator
 from django.core.exceptions import ValidationError
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 from django.utils import timezone
 
 from .validators import validate_username
